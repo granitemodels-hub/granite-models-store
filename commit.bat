9a417f0b93852d45ba3833eb209efac27ec02a71
GMA <admin@granitemodels.local>
2026-04-12 21:15:06 -0400

    Remove temp commit.bat

--- commit.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d C:\Users\Axelf\OneDrive\GRANITE-FINAL\deployed-store
-git add -A
-git commit -m "Shrink hero logo 420 to 260, tighten h1 margin, kill black void"
-git push origin main
